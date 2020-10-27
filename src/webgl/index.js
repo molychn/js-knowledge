9ac68f9f85b037678f057807484ca5d8b479189c
molychn <ssomkhyy@qq.com>
2020-10-27 14:43:55 +0800

    webgl基础着色器

---
+++ src/webgl/index.js
@@ -0,0 +1,168 @@
+// 顶点着色器
+const vsSource = `
+  attribute vec4 aVertexPosition;
+
+  uniform mat4 uModelViewMatrix;
+  uniform mat4 uProjectionMatrix;
+
+  void main() {
+    gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+  }
+`
+
+// 片段着色器
+const feSource = `
+  void main() {
+    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+  }
+`
+
+function main() {
+  const canvas = document.querySelector('#gl')
+  // const canvas = document.createElement('canvas')
+  // document.querySelector('body').appendChild(canvas)
+  const gl = canvas.getContext("webgl")
+
+  if (!gl) {
+    alert(2333)
+    return
+  }
+
+  // gl.clearColor(0.0, 0.0, 0.0, 1.0)
+  // gl.clear(gl.COLOR_BUFFER_BIT)
+  const shaderProgram = initShaderprogram(gl, vsSource, feSource)
+  // 创建programInfo
+  const programInfo = {
+    program: shaderProgram,
+    attribLocations: {
+      vertexPosition: gl.getAttribLocation(shaderProgram, 'aVertexPosition')
+    },
+    uniformLocations: {
+      projectionMatrix: gl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
+      modelViewMatrix: gl.getUniformLocation(shaderProgram, 'uModelViewMatrix')
+    }
+  }
+  const buffer = initBuffers(gl)
+  drawScene(gl, programInfo, buffer)
+}
+
+
+
+// 初始化着色程序
+function initShaderprogram(gl, vsSource, feSource) {
+  const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource)
+  const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, feSource)
+
+  // 创建着色器程序
+  const shaderProgram = gl.createProgram()
+  gl.attachShader(shaderProgram, vertexShader)
+  gl.attachShader(shaderProgram, fragmentShader)
+  gl.linkProgram(shaderProgram)
+
+  // 创建失败alert弹框
+  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+    alert(gl.getProgramInfoLog(shaderProgram))
+    return null
+  }
+
+  return shaderProgram
+}
+
+// 根据指定类型创建着色器，上传source源码编译
+function loadShader(gl, type, source) {
+  // 根据类型创建相应的shader
+  const shader = gl.createShader(type)
+  // 编译source
+  gl.shaderSource(shader, source)
+  gl.compileShader(shader)
+
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    alert(gl.getShaderInfoLog(shader))
+    gl.deleteShader(shader)
+    return null
+  }
+
+  return shader
+}
+
+function initBuffers(gl) {
+  const positionBuffer = gl.createBuffer()
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
+
+  let vertices = [
+    1.0,  1.0,  0.0,
+    -1.0, 1.0,  0.0,
+    1.0,  -1.0, 0.0,
+    -1.0, -1.0, 0.0
+  ]
+
+  gl.bufferData(gl.ARRAY_BUFFER,
+                new Float32Array(vertices),
+                gl.STATIC_DRAW)
+
+  return {
+    position: positionBuffer
+  }
+}
+
+function drawScene(gl, programInfo, buffers) {
+  // 清除并填充颜色
+  gl.clearColor(0.0, 0.0, 0.0, 1.0)
+  gl.clearDepth(1.0)
+  gl.enable(gl.DEPTH_TEST)
+  gl.depthFunc(gl.LEQUAL)
+
+  // 清除canvas缓存
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
+  const fieldOfView = 45 * Math.PI / 180
+  const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight
+  const zNear = 0.1
+  const zFar = 100.0
+  const projectionMatrix = mat4.create()
+  mat4.perspective(projectionMatrix, fieldOfView, aspect, zNear, zFar)
+  const modelViewMatrix = mat4.create()
+  mat4.translate(modelViewMatrix, modelViewMatrix, [-0.0, 0.0, -6.0])
+  
+  // Tell WebGL how to pull out the positions from the position
+  // buffer into the vertexPosition attribute.
+  {
+    const numComponents = 3;  // pull out 3 values per iteration
+    const type = gl.FLOAT;    // the data in the buffer is 32bit floats
+    const normalize = false;  // don't normalize
+    const stride = 0;         // how many bytes to get from one set of values to the next
+                              // 0 = use type and numComponents above
+    const offset = 0;         // how many bytes inside the buffer to start from
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
+    gl.vertexAttribPointer(
+        programInfo.attribLocations.vertexPosition,
+        numComponents,
+        type,
+        normalize,
+        stride,
+        offset);
+    gl.enableVertexAttribArray(
+        programInfo.attribLocations.vertexPosition);
+  }
+
+  // Tell WebGL to use our program when drawing
+
+  gl.useProgram(programInfo.program);
+
+  // Set the shader uniforms
+
+  gl.uniformMatrix4fv(
+      programInfo.uniformLocations.projectionMatrix,
+      false,
+      projectionMatrix);
+  gl.uniformMatrix4fv(
+      programInfo.uniformLocations.modelViewMatrix,
+      false,
+      modelViewMatrix);
+
+  {
+    const offset = 0;
+    const vertexCount = 4;
+    gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
+  }
+}
