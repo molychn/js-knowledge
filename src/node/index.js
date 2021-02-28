cd78e4f19b0708fee139e31fdb609d39c410a3e0
molychn <15766227859@163.com>
2021-02-28 21:49:48 +0800

    node测试

--- src/node/index.js
+++ src/node/index.js
@@ -1,8 +1,25 @@
-// process.stdin.pipe(process.stdout)
-// console.log(process.argv)
-process.nextTick(console.log, 4);
-setImmediate(console.log, 1);
-setTimeout(console.log, 1, 2);
-Promise.resolve(3).then(console.log);
+const fs = require('fs')
 
-console.log(5);
+const rs = fs.createReadStream('./test.txt', {
+  start: 11,
+  end: 36
+})
+
+rs.on('open', fd => {
+  console.log(`file desc char ${fd} set`)
+})
+rs.on('ready', () => {
+  console.log('file ready')
+})
+rs.on('data', chunk => {
+  console.log(`file data: ${chunk.toString()}`)
+})
+rs.on('end', () => {
+  console.log('file read finish')
+})
+rs.on('close', () => {
+  console.log('file close')
+})
+rs.on('error', err => {
+  console.log(`error: ${err.stack()}`)
+})
