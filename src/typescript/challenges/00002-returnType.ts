e9058055b1ced326e771b3a9d79a9bfe83bfaac1
molychn <15766227859@163.com>
2022-06-09 09:59:05 +0800

    Create 00002-returnType.ts

---
+++ src/typescript/challenges/00002-returnType.ts
@@ -0,0 +1,10 @@
+const fn = (v: boolean) => {
+  if (v)
+    return 1
+  else
+    return 2
+}
+
+type MyReturnType<T> = T extends (...args: any[]) => infer R ? R : never
+
+type type2 = MyReturnType<typeof fn>
